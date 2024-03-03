891069435c950e2504a83e1430f636cfa35b7403
joel-kalema <joelkalema63@gmail.com>
2024-03-03 03:02:01 +0200

    added header pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,12 +3,12 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <>
-      <section className="home">
+      {/* <section className="home">
 
       </section>
       <section className="sm:mt-12">
 
-      </section>
+      </section> */}
     </>
   );
 }
