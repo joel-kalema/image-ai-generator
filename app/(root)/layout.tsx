7bfaf0b94aafdcd253a8f8d460a5a7380162cbee
joel-kalema <joelkalema63@gmail.com>
2024-03-15 13:12:46 +0200

    update image actions

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
-// import { Toaster } from '@/components/ui/toaster'
+import { Toaster } from '@/components/ui/toaster'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -14,9 +14,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </div>
       
-      {/* <Toaster /> */}
+      <Toaster />
     </main>
   )
 }
 
-export default Layout;
+export default Layout
